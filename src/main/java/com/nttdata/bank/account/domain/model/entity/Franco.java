34fb66f413c6c2e253aad4e4e6c8d23160056a41
FrancoGT <francogt8000@gmail.com>
2023-09-25 20:45:33 -0500

    Clase Franco

---
+++ src/main/java/com/nttdata/bank/account/domain/model/entity/Franco.java
@@ -0,0 +1,17 @@
+package src.main.java.com.nttdata.bank.account.domain.model.entity;
+
+public class Franco 
+{
+	private String nombre;
+
+	public String getNombre() 
+	{
+		return nombre;
+	}
+
+	public void setNombre(String nombre) 
+	{
+		this.nombre = nombre;
+	}
+	
+}
