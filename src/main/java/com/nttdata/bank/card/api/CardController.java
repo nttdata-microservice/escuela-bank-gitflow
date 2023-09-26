bac6e8d76762d6a7b8a46eeb7c603964dcce3b67
Juan Flores Moroco <pcsijflo@upc.edu.pe>
2023-09-25 22:06:56 -0500

    chore: fixing observations

--- src/main/java/com/nttdata/bank/card/api/CardController.java
+++ src/main/java/com/nttdata/bank/card/api/CardController.java
@@ -1,4 +1,10 @@
 package com.nttdata.bank.card.api;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CardController {
+  public void getData() {
+    System.out.println("Hello");
+  }
 }
--- src/main/java/com/nttdata/bank/card/service/CardServiceImpl.java
+++ src/main/java/com/nttdata/bank/card/service/CardServiceImpl.java
@@ -1,6 +1,11 @@
 package com.nttdata.bank.card.service;
 
+import com.nttdata.bank.card.domain.model.document.Card;
 import com.nttdata.bank.card.domain.service.CardService;
 
 public class CardServiceImpl implements CardService {
+  @Override
+  public void save(Card card) {
+
+  }
 }
