7a0f27a10c068cff5a3ed0669a5bde8320f1b621
Manuel Paz <mpaz120478@gmail.com>
2023-09-25 20:55:53 -0500

    chore: adding new entity ManuelPaz

---
+++ src/main/java/com/nttdata/bank/account/domain/model/entity/ManuelPaz.java
@@ -0,0 +1,11 @@
+package com.nttdata.bank.account.domain.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ManuelPaz {
+    private Long id;
+    private String name;
+}
