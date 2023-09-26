4d68896025ce5659d944f3274d12a1c3d8667327
melany <melanyfabiola16@gmail.com>
2023-09-25 20:24:27 -0500

    Adding/Change new Entity MelanyQuispe

---
+++ src/main/java/com/nttdata/bank/account/domain/model/entity/MelanyQuispe.java
@@ -0,0 +1,13 @@
+package com.nttdata.bank.account.domain.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+public class MelanyQuispe {
+
+    private Integer id;
+}
